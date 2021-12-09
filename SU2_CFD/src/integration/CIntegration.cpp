18fbb39c049262bcfa1e608bd6a9962592dcdd24
Davide Gatti <davide@LAPTOP-GK2H5HQD.localdomain>
2021-12-09 01:37:20 +0100

    beak missing

--- SU2_CFD/src/integration/CIntegration.cpp
+++ SU2_CFD/src/integration/CIntegration.cpp
@@ -156,6 +156,7 @@ void CIntegration::Space_Integration(CGeometry *geometry,
         break;
       case BLOWING:
 	solver_container[MainSolver]->BC_Isothermal_Wall_Blowing(geometry, solver_container, conv_bound_numerics, visc_bound_numerics, config, iMarker);
+	break;
       case HEAT_FLUX:
         solver_container[MainSolver]->BC_HeatFlux_Wall(geometry, solver_container, conv_bound_numerics, visc_bound_numerics, config, iMarker);
         break;
